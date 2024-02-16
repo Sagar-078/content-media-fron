2372dcab32bbdacf98cbed8aa09700b57658ccd9
Sagar-078 <139355166+Sagar-078@users.noreply.github.com>
2024-02-16 17:18:09 +0530

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,6 +4,7 @@ import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import Loader from './Loader';
 
 const LoginForm = (Props) => {
 
@@ -41,23 +42,31 @@ const LoginForm = (Props) => {
     
         }catch(err){
           toast.error("Invalid Id & Password !!");
+            setLoading(false);
           console.log(err);
         }
     
     }
 
   return (
-    <div className="bg-slate-400 w-[30%] max-lg:w-[38%] max-md:w-[50%] max-sm:w-[80%] flex justify-center items-center p-5 rounded-lg flex-col gap-8">
-       <form className="w-full flex justify-center flex-col items-center gap-5" >
-          <h1 className="text-white font-bold font-mono text-lg">Login</h1>
-          <input type="email" onChange={(e) =>setEmail(e.target.value)} placeholder="mail id :" className="px-2 py-2 rounded backgroung placeholder:text-slate-200 border-none outline-none w-[70%]"/>
-          <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password :" className="px-2 py-2 rounded backgroung placeholder:text-slate-200 border-none outline-none w-[70%]"/>
-          <button className="bg-slate-800 text-white px-4 py-2 rounded font-bold" onClick={submitHandler} >Submit</button>
-        </form>
-        <div className="flex flex-row gap-7 items-baseline">
+    <div className="w-full flex items-center justify-center">
+        {
+            loading ? (<Loader/>) : 
+            (
+                <div className="bg-slate-400 w-[30%] max-lg:w-[38%] max-md:w-[50%] max-sm:w-[80%] flex justify-center items-center p-5 rounded-lg flex-col gap-8">
+               <form className="w-full flex justify-center flex-col items-center gap-5" >
+                <h1 className="text-white font-bold font-mono text-lg">Login</h1>
+              <input type="email" onChange={(e) =>setEmail(e.target.value)} placeholder="mail id :" className="px-2 py-2 rounded backgroung placeholder:text-slate-200 border-none outline-none w-[70%]"/>
+              <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password :" className="px-2 py-2 rounded backgroung placeholder:text-slate-200 border-none outline-none w-[70%]"/>
+              <button className="bg-slate-800 text-white px-4 py-2 rounded font-bold" onClick={submitHandler} >Submit</button>
+            </form>
+            <div className="flex flex-row gap-7 items-baseline">
             <p className="font-semibold text-sm">Don't have account ?</p>
             <button className="text-white font-bold" onClick={() => {setSign(false)}}>Sign Up</button>
-        </div> 
+            </div> 
+            </div>
+            )
+        }
     </div>
   )
 }
